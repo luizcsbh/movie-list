c6f6d9ed2be150b390327303ed9543af2d6c503d
Luiz Santos <luizcsbh@gmail.com>
2021-06-08 16:34:53 -0300

    Subindo o projeto

---
+++ src/api/MoviesServices.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+const API_KEY = 
+
+export class MoviesServices
---
+++ src/views/Home.js
@@ -0,0 +1,11 @@
+import { Movie } from '../components/Movie';
+
+export const Home = () => (
+    <div className="container">
+        <div className="row gy-5">
+            <div className="col-3">
+                <Movie />
+            </div>
+        </div>
+    </div>
+)
